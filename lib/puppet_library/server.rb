b14e2adb2e6df674f0f608953af8b8696cd3ab69
drrb <drrrrrrrrrrrb@gmail.com>
2013-12-27 21:19:49 +1100

    Moved imports

--- lib/puppet_library/server.rb
+++ lib/puppet_library/server.rb
@@ -14,11 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 require 'sinatra/base'
 require 'json'
-require 'rubygems/package'
-require 'zlib'
 
 require 'puppet_library/module_repo'
 
