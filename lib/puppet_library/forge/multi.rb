ec01940fe7ef79bf9b865555c98d1633aa409686
drrb <drrrrrrrrrrrb@gmail.com>
2014-01-24 20:27:03 +1100

    Cleaned up search implementation

--- lib/puppet_library/forge/multi.rb
+++ lib/puppet_library/forge/multi.rb
@@ -19,8 +19,7 @@ class Array
     def version_sort_on
         sort_by do |element|
             version = yield(element)
-            #TODO: this fails for 1.1.0rc1
-            version.split('.').map {|part| part.to_i}
+            Gem::Version.new(version)
         end
     end
 end
@@ -32,36 +31,16 @@ module PuppetLibrary::Forge
         end
 
         def search_modules(query)
-            results = {}
-            forges.each do |forge|
-                search_results = forge.search_modules(query)
-                search_results.each do |result|
-                    module_results = results[result["full_name"]] ||= []
-                    module_results << result
-                end
+            all_results = forges.map do |forge|
+                forge.search_modules(query)
+            end.flatten
+
+            results_by_module = all_results.group_by do |result|
+                result["full_name"]
             end
-            results.values.map do |module_results|
-                max_version = module_results.max_by do |r|
-                    r["version"]
-                end["version"]
 
-                tags = module_results.inject([]) do |tags, result|
-                    result_tags = result["tag_list"] || []
-                    (tags + result_tags).uniq
-                end
-
-                releases = module_results.inject([]) do |releases, result|
-                    result_releases = result["releases"] || []
-                    (releases + result_releases).uniq
-                end.version_sort_on do |release|
-                    release["version"]
-                end.reverse
-
-                res = module_results.first
-                res["version"] = max_version
-                res["tag_list"] = tags
-                res["releases"] = releases
-                res
+            results_by_module.values.map do |module_results|
+                combine_search_results(module_results)
             end.flatten
         end
 
@@ -110,6 +89,26 @@ module PuppetLibrary::Forge
         private
         def forges
             @forges ||= []
+        end
+
+        def combine_search_results(search_results)
+            highest_version, tags, releases = search_results.inject([nil, [], []]) do |(highest_version, tags, releases), result|
+                [
+                    max_version(highest_version, result["version"]),
+                    tags + (result["tag_list"] || []),
+                    releases + (result["releases"] || [])
+                ]
+            end
+
+            combined_result = search_results.first.tap do |result|
+                result["version"] = highest_version
+                result["tag_list"] = tags.uniq
+                result["releases"] = releases.uniq.version_sort_on {|r| r["version"]}.reverse
+            end
+        end
+
+        def max_version(left, right)
+            [Gem::Version.new(left), Gem::Version.new(right)].max.version
         end
     end
 end
