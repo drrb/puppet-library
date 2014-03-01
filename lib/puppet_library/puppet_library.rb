f53151900c9434717aa1fc9737b40d415c0100be
drrb <drrrrrrrrrrrb@gmail.com>
2014-03-01 16:37:22 +1100

    Moved import

--- lib/puppet_library/puppet_library.rb
+++ lib/puppet_library/puppet_library.rb
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'optparse'
 require 'rack'
 require 'yaml'
 require 'puppet_library/forge/directory'
